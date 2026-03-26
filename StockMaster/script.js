ac179509d50bf7436cf93c36bec967b0e93f4e19
neoisaacui <neoisaacg@gmail.com>
2026-03-26 08:00:43 +0800

    Add files via upload

---
+++ StockMaster/script.js
@@ -0,0 +1,34 @@
+function login(){
+
+let user = document.getElementById("username").value
+let pass = document.getElementById("password").value
+
+if(user === "admin" && pass === "1234"){
+window.location = "dashboard.html"
+}
+else{
+alert("Invalid login")
+}
+
+}
+
+function searchOrders() {
+
+let input = document.getElementById("searchOrders");
+let filter = input.value.toLowerCase();
+let table = document.getElementById("ordersTable");
+let tr = table.getElementsByTagName("tr");
+
+for (let i = 1; i < tr.length; i++) {
+
+let rowText = tr[i].textContent.toLowerCase();
+
+if (rowText.includes(filter)) {
+tr[i].style.display = "";
+} else {
+tr[i].style.display = "none";
+}
+
+}
+
+}
